973748e5ee9fad1d381c5d0af5733dc051d193bf
yah96 <yahya.alhousein@std.balamand.edu.lb>
2024-02-15 12:50:41 +0300

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -54,8 +54,7 @@ const App = () => {
         <Routes>
           <Route path="/admin" element={<Login setUser={setUser} />} />
           <Route path="/upload-images" element={user ? <ImageUpload /> : <Navigate to="/admin" />} />
-          <Route path="/" element={<LoadImages />} />
-          <Route index element={<Navigate to="/load-images" />} />
+          <Route path="/DaliasAccessories" element={<LoadImages />} />
           {/* Pass addToCart function as a prop to DynamicImageDisplay */}
           <Route path="/category/:mainCategory/:subCategory" element={<DynamicImageDisplay updateCartItemsCount ={updateCartItemsCount } />} />
           <Route path="/category/:mainCategory/" element={<MainCategoryComponent />} />
