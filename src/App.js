7b4d608b1ddce0111a75c2711ebda74372ed1997
yah96 <yahya.alhousein@std.balamand.edu.lb>
2024-02-15 23:23:02 +0300

    updated hashrouter instead of browserrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase';
 import ImageUpload from './components/ImageUpload';
